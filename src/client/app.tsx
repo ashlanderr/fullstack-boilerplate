4657414b3983c22b9873a201dec07d4495061584
Alexander Shilov <aleksandr.schilow2012@gmail.com>
2026-04-20 19:27:51 +0300

    tmp 4

--- src/client/app.tsx
+++ src/client/app.tsx
@@ -1,7 +1,13 @@
 import {add} from "../shared/utils.ts";
+import {useState} from "react";
+import type {Foo} from "../server/main.tsx";
 
 function App() {
   add(22, 33);
+  const [state,] = useState<Foo>({foo: 'aaa', bar: 11});
+
+  console.log(state);
+
   return <div>Hello World111222!</div>
 }
 
--- src/server/main.tsx
+++ src/server/main.tsx
@@ -1,5 +1,10 @@
 import {readFileSync} from 'node:fs';
 import {add} from "../shared/utils.ts";
 
+export interface Foo {
+    foo: string;
+    bar: number;
+}
+
 add(11, 22)
 console.log(readFileSync('package.json', 'utf8'));
