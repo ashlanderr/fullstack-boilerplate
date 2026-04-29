968de20eef5ada4674633c4e26b7e4ff9d67cff3
Alexander Shilov <aleksandr.schilow2012@gmail.com>
2026-04-29 20:13:53 +0300

    Add prettier eslint plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
 import js from "@eslint/js";
 import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
+import prettier from "eslint-plugin-prettier/recommended";
 
 export default defineConfig([
   globalIgnores(["dist"]),
@@ -13,10 +13,20 @@ export default defineConfig([
       js.configs.recommended,
       tseslint.configs.recommended,
       reactRefresh.configs.vite,
+      prettier,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
     },
   },
 ]);
